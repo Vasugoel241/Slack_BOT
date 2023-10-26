3b4c1cca6929f30a10be51036803556ab73bed67
Vasu Goel <vasugoel241@gmail.com>
2023-10-26 18:36:07 +0530

    updated app

--- slack_message.py
+++ slack_message.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 import logging
-logging.basicConfig(filename='app.log',level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
 from slack_sdk import WebClient
 from slack_sdk.signature import SignatureVerifier
