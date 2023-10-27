a4dfabf815eeda5865885646a8797c079c986a33
Vasu Goel <vasugoel241@gmail.com>
2023-10-27 15:22:50 +0530

    removed test files

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 from flask import Flask,request, jsonify
-# import logging 
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
 import os
 from dotenv import load_dotenv
-# import logging
-# logging.basicConfig(level=logging.DEBUG)
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
-from slack_sdk import WebClient
-# from slack_sdk.signature import SignatureVerifier
-# from slack_sdk.web import SlackResponse
-from slack_sdk.errors import SlackApiError
 
 env_path = ".env"
 load_dotenv(env_path)
 
 slack_token = os.environ['SLACK_BOT_TOKEN']
 signing_secret = os.environ['SIGNING_SECRET']
+webhook_url = os.environ['SLACK_WEBHOOK_URL']
 
 slack_client = WebClient(token=slack_token)
 
 app = Flask(__name__)
+
 # logging.basicConfig(level=logging.DEBUG)
 
-@app.route("/slack/message_actions", methods=["POST"])
+@app.route("/slack/post-message", methods=["POST"])
 def slack_message_actions():
     event_data = request.get_json()
     try:
@@ -40,5 +40,7 @@ if __name__ == "__main__":
         print ("SLACK_BOT_TOKEN env variable is not defined")
     elif os.environ.get('CHATBOT_NAME') is None:
         print ("CHATBOT_NAME env variable is not defined")
+    elif os.environ.get('SLACK_WEBHOOK_URL') is None:
+        print ("SLACK_WEBHOOK_URL env variable is not defined")
    
-    app.run(debug = True, host='0.0.0.0')
+    app.run(debug = True, host='0.0.0.0', port=5000)
